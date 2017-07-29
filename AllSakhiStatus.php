e7f5fee1778841bbbcf05c0b3d0e3ccdfa630fe8
aayushshah96 <aayush1996@hotmail.com>
2017-07-29 18:51:54 +0530

    Ordered by location

--- AllSakhiStatus.php
+++ AllSakhiStatus.php
@@ -5,12 +5,19 @@
     
 ?>
 <html>
-    <head><title></title></head>
+    <head>
+        <title></title>
+    <style>
+        table, th, td {
+    border: 1px solid black;
+}
+        </style>
+    </head>
     <body>
         <?php
         $sakhilocation="Goregaon";
     //$qry1="select * from sakhi where location='".$sakhilocation."';";
-    $qry2="select * from itemsdetails;";
+    $qry2="select * from itemsdetails order by sakhiloc;";
     //echo $qry;
     $result=mysqli_query($conn,$qry2);
     //echo $result;
@@ -21,13 +28,13 @@
 	 echo "Successful";
 } else {
 	echo "Query error"; 
-}*/echo "<table><tr><th>Name</th><th>Item</th><th>Quantity</th></tr>";
+}*/echo "<table><tr><th>Name</th><th>Breakfast</th><th>Methi Masala</th><th>Dahi Bajri Methi</th><th>Nachni</th><th>Jeera</th><th>Peppery Oats</th><th>Khichdi</th><th>Punjabi Masala</th><th>Low Calorie</th></tr>";
         if ($result->num_rows > 0) {
 							     // output data of each row
 							     while($row = $result->fetch_assoc()) {
 							     	//echo "<tr>";
                                     // echo "<tr><td>Test</td></tr>";
-							         echo "<tr><td>" . $row["sakhiid"] . "</td> <td>" . $row["iname"] ."</td><td> ".$row["quantity"]." </td></tr>";
+							         echo "<tr><td>" . $row["sakhiid"] . "</td> <td> ".$row["quantity1"]."kg</td><td> ".$row["quantity2"]."kg</td><td> ".$row["quantity3"]."kg</td><td> ".$row["quantity4"]."kg</td><td> ".$row["quantity5"]."kg</td><td> ".$row["quantity6"]."kg</td><td>".$row["quantity7"]."kg</td><td> ".$row["quantity8"]."kg</td><td> ".$row["quantity9"]."kg</td></tr>";
 							         //echo "<img src='/php_test/image_archive/" . $last_file[0] . "' alt='error'>";
 
 
