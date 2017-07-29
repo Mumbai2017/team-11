1902de79ddb5989d72099015331f511eae8802d7
Maulik003 <joshimaulikp@gmail.com>
2017-07-29 22:43:35 +0530

    added dashboard and volunteer file

---
+++ template/sakhi_display.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<?php include'connection.php'
+session_start();
+ ?>
+ 
+<html lang="en">
+	<head>
+		
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="mystyle.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+	</head>
+	<body>
+<img src="sanisaheader.jpg" style="width:100%";></img>
+<div class="topnav" id="myTopnav">
+  <a href="index.php">Home</a>
+  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
+<div class="container">
+  <h2>Sakhis near you</h2>
+                                                                                       
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Sakhi Name</th>
+        <th>Breakfast Khakra</th>
+		<th>Methi Masala Khakra</th>
+        <th>Dahi Bajri Methi</th>
+		<th>Nachni Khakra</th>
+        <th>Jeera Khakra</th>
+		<th>Peppery Oats Khakra</th>
+		<th>Khichdi Khakra</th>
+		<th>Punjabi Khakra</th>
+		<th>Low Calorie Khakra</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
