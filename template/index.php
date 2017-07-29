cd76b93f286cd1a59ca387343cbed12c552823ca
Maulik003 <joshimaulikp@gmail.com>
2017-07-29 18:49:22 +0530

    added body

---
+++ template/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<?php include'connection.php'
+session_start();
+ ?>
+ 
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Transit by TEMPLATED</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body class="landing">
+
+		<header id="header" style="background-color:black";>
+				<h1><a href="index.html">Welcome!</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="signup.html" id="register" class="button special">Register</a></li>
+						<li><a href="Login.html" id="login" class="button special">Sign In</a></li>
+					</ul>
+				</nav>
+			</header>
+		
+		<
+		<!-- Banner -->
+			<section id="banner">
+				<h2>Shramak Naari Sangh</h2>
+				<p>Priviledged are those who can support the less priviledged!</p>
+				<ul class="actions">
+					<!--<li>
+						<a href="#" class="button big">Lorem ipsum dolor</a>
+					</li>-->
+				</ul>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 special">
+				<div class="container">
+					<header class="major">
+						<h2>Some of our chakri products are</h2>
+
+					</header>
+					<div class="row 150%">
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Breakfast Khakra</h3>
+								<img src="images/breakfast.jfif" alt="Mountain View" style="width:304px;height:228px;">
+						</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Methi Khakra</h3>
+								<img src="images/methi.jfif" alt="Mountain View" style="width:304px;height:228px;">
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Khichdi Khakra</h3>
+								<img src="images/khichadi.jfif" alt="Mountain View" style="width:304px;height:228px;">
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Jeera Khakra</h3>
+								<img src="images/jerra.jfif" alt="Mountain View" style="width:304px;height:228px;">
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Peppery Oats Khakra</h3>
+								<img src="images/peppery.jfif" alt="Mountain View" style="width:304px;height:228px;">
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								
+								<h3>Punjabi Masala Khakra</h3>
+								<img src="images/punjabi.jfif" alt="Mountain View" style="width:304px;height:228px;">
+							</section>
+						</div>
+					</div>
+				</div>
+				<footer>
+						<p>100% Whole Wheat, No Maida, No White Sugar, Zero Transfat, No Preservatives!</p>
+						
+					</footer>
+			</section>
+
+
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+				<h2>Contact Us!</h2>
+					<section class="links">
+						<div class="row">
+						
+							<section class="3u 6u(medium) 12u$(small)">
+								<h3>Registered Office</h3>
+								<ul class="unstyled">
+									<li><a href="#">Sharda Chambers, 15,</a></li>
+									<li><a href="#">702, 7th Floor,</a></li>
+									<li><a href="#">New Marine Lines,</a></li>
+									<li><a href="#">Mumbai 400020</a></li>
+									<li><a href="#">Tel : +91 -22 – 3254 2395</a></li>
+								</ul>
+							</section>
+							<section class="3u 6u$(medium) 12u$(small)">
+								<h3>Distribution Depot</h3>
+								<ul class="unstyled">
+									<li><a href="#">Chaakri, No. 4, Opp. Navbharat School,</a></li>
+									<li><a href="#">Netaji Subhash Road,</a></li>
+									<li><a href="#">Mulund West,</a></li>
+									<li><a href="#">Mumbai 400080</a></li>
+									<li><a href="#">IndiaTel : 91 – 22 – 3256 4498</a></li>
+								</ul>
+							</section>
+							<section class="3u 6u(medium) 12u$(small)">
+								<h3>Contacts</h3>
+								<ul class="unstyled">
+									<li><a href="#">Chaakri: Bosky Bavisi : +91 9029234567</a></li>
+									<li><a href="#">Puro: Priyal Parikh : +91 9820395557</a></li>
+									<li><a href="#">Divinite: Prachi Shah : +91 9820896800</a></li>
+									<li><a href="#">Udaan: Bhavna Parikh : +91 9820085033</a></li>
+									
+								</ul>
+							</section>
+							<!--<section class="3u$ 6u$(medium) 12u$(small)">
+								<h3>Illum, tempori, saepe</h3>
+								<ul class="unstyled">
+									<li><a href="#">Lorem ipsum dolor sit</a></li>
+									<li><a href="#">Recusandae, culpa necessita nam</a></li>
+									<li><a href="#">Cupiditate, debitis adipisci blandi</a></li>
+									<li><a href="#">Tempore nam, enim quia</a></li>
+									<li><a href="#">Explicabo molestiae dolor labore</a></li>
+								</ul>
+							</section>-->
+						</div>
+					</section>
+					<div class="row">
+						<div class="8u 12u$(medium)">
+							<ul class="copyright">
+								<li>© 2010-2017 Sa-Ni-Sa.  All rights reserved.</li>
+								
+							</ul>
+						</div>
+						<div class="4u$ 12u$(medium)">
+							<ul class="icons">
+								<li>
+									<a class="icon rounded fa-facebook"><span class="label">Facebook</span></a>
+								</li>
+								<li>
+									<a class="icon rounded fa-twitter"><span class="label">Twitter</span></a>
+								</li>
+								<li>
+									<a class="icon rounded fa-google-plus"><span class="label">Google+</span></a>
+								</li>
+								<li>
+									<a class="icon rounded fa-linkedin"><span class="label">LinkedIn</span></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+	</body>
+</html>
