fa659da25dddbe6903788a94f022b21af5a22c7d
aayushshah96 <aayush1996@hotmail.com>
2017-07-30 10:19:57 +0530

    Final Changes

--- order_confirm.php
+++ order_confirm.php
@@ -15,7 +15,34 @@
   	$item_name = $_POST["pname"];
   	#$sql = "UPDATE itemsdetails SET quantity=quantity-'$quantity' WHERE iname = '$item_name'";
   	#$result = mysqli_query($conn,$sql);
-  	header("Location: myorders.php");
+      $post_data = array(
+    // 'From' doesn't matter; For transactional, this will be replaced with your SenderId;
+    // For promotional, this will be ignored by the SMS gateway
+    'From'   => 'xxxxxxxxxx',
+    'To'    => '9029912132',
+    'Body'  => 'Hi Aayush, your number 9123456780 is now turned confirmed.',
+);
+ 
+$exotel_sid = "codeforgood7"; // Your Exotel SID
+$exotel_token = "6d45b2cbec38dc28a3bb0c4c5ed2a646632f28e4"; // Your exotel token
+ 
+$url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+ 
+$http_result = curl_exec($ch);
+$error = curl_error($ch);
+$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+ 
+curl_close($ch);
+  	header("Location: track_sakhi.html");
   }
   else {
   	$sql = "UPDATE orderdetails SET sakhi_id = 0 WHERE sakhi_id = '$sakhi_id'";
