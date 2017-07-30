463dda45dcbe1447bfb0c3a61f320222a1c8bf7b
Maulik003 <joshimaulikp@gmail.com>
2017-07-30 05:59:38 +0530

    editted

--- processing.php
+++ processing.php
@@ -1,1 +1,37 @@
-Hello World
+
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<?php include'connection.php'
+session_start();
+ ?>
+ 
+<html lang="en">
+	<head>
+		
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="mystyle.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+	</head>
+	<body>
+<img src="sanisaheader.jpg" style="width:100%";></img>
+
+<pre> <h1> Your is being processed...</h1>
+Your order will be fulfilled by the availabe Sakhi at the earliest.
+Please check back for more datails.
+
+</pre>
+</body>
+</html>
